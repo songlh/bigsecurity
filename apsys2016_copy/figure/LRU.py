798bb5b60249c52964aee20e59b745c7980cda98
wenfei-wu <wuwenfeiwwf@gmail.com>
2016-05-20 04:49:23 -0700

    new file:   Makefile 	new file:   algorithm2e.sty 	new file:   clean.sh 	new file:   cleveref.sty 	new file:   couriers.sty 	new file:   double-blind.sty 	new file:   etoolbox.sty 	new file:   figure/FP.py 	new file:   figure/LRU.day.pdf 	new file:   figure/LRU.pdf 	new file:   figure/LRU.py 	new file:   figure/LRU.result 	new file:   figure/LRU_day.pdf 	new file:   figure/aveUncover.pdf 	new file:   figure/aveUncover.py 	new file:   figure/cum.pdf 	new file:   figure/day.LRU.result 	new file:   figure/day_LRU.py 	new file:   figure/dist.py 	new file:   figure/fp.pdf 	new file:   figure/maxError.pdf 	new file:   figure/maxError.py 	new file:   figure/maxUncover.pdf 	new file:   figure/maxUncover.py 	new file:   figure/new.family.result 	new file:   figure/new_family.pdf 	new file:   figure/new_family.py 	new file:   figure/nov.data.txt 	new file:   figure/nov.pdf 	new file:   figure/nov.py 	new file:   figure/overlap.pdf 	new file:   figure/overlap.py 	new file:   figure/tag.total.result 	new file:   main.tex 	new file:   oopsla2016.sty 	new file:   section/0_intro.tex 	new file:   section/1_data.tex 	new file:   section/2_1_LRU.tex 	new file:   section/2_2_item.tex 	new file:   section/4_opp.tex 	new file:   section/5_related.tex 	new file:   section/abstract.tex 	new file:   shared-affiliation.sty 	acm_proc_article-sp.cls 	macros.tex

---
+++ apsys2016_copy/figure/LRU.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from matplotlib.ticker import FixedLocator
+import numpy as np
+
+with open('./LRU.result') as f:
+	lines = f.read().splitlines()
+
+entryList = []
+accurateList = []
+
+for line in lines:
+	tmp = line.split()
+	accurateList.append(float(tmp[3]) * 100)
+	entryList.append(int(tmp[0]))
+
+
+#for i in rateList:
+#	print i, accumList[i]
+
+fig, ax = plt.subplots()
+ax.plot(entryList, accurateList, 'b-x',linewidth=2.0, markersize=5, mew=2)
+
+
+major_ticks = np.arange(0, 1001, 100) 
+
+
+xmin = -1
+xmax = 1001
+ax.set_xlim(xmin, xmax)
+#xticks = ax.xaxis.get_major_ticks()
+#xticks[0].label1.set_visible(False)
+ax.set_xticks(major_ticks)
+plt.xlabel('The number of cache entries', fontsize=18)
+
+ymin = 0
+ymax = 101
+ax.set_ylim(ymin, ymax)
+plt.ylabel('Cache hit rates (%)',  fontsize=18)
+ax.xaxis.set_tick_params(labelsize=16)
+ax.yaxis.set_tick_params(labelsize=16)
+
+ax.grid(True)
+#plt.title('Cumulative frequencies')
+#plt.show()
+fig.savefig('LRU.pdf')
+plt.close(fig)
---
+++ apsys2016_copy/figure/maxError.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from matplotlib.ticker import FixedLocator
+import numpy as np
+
+phiList = [10, 100, 1000]
+overlapList = [2178, 999, 10]
+
+
+
+#for i in rateList:
+#	print i, accumList[i]
+
+fig, ax = plt.subplots()
+ax.plot(phiList, overlapList, 'b-x', linewidth=2.0, markersize=14, mew=4)
+
+
+#major_ticks = np.arange(0, 101, 10) 
+
+
+xmin = 9
+xmax = 1100
+ax.set_xlim(xmin, xmax)
+#xticks = ax.xaxis.get_major_ticks()
+#xticks[0].label1.set_visible(False)
+for tick in ax.xaxis.get_major_ticks():
+	tick.label.set_fontsize(18) 
+ax.set_xscale("log", nonposx='clip')
+plt.xlabel('$\phi$', fontsize=24)
+
+ymin = 1
+ymax = 2500
+ax.set_ylim(ymin, ymax)
+for tick in ax.yaxis.get_major_ticks():
+	tick.label.set_fontsize(18) 
+ax.set_yscale("log", nonposx='clip')
+plt.ylabel('maxError',  fontsize=24)
+
+plt.gcf().subplots_adjust(bottom=0.15)
+plt.gcf().subplots_adjust(left=0.15)
+ax.grid(True)
+#plt.title('Cumulative frequencies')
+#plt.show()
+fig.savefig('maxError.pdf')
+plt.close(fig)
