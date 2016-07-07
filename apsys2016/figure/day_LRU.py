d5e0014cc911cd302569538335cd32355c9c9c7e
songlh <songlinhai0543@gmail.com>
2016-07-07 01:01:00 -0700

    check in updates in section 4

--- apsys2016/figure/day_LRU.py
+++ apsys2016/figure/day_LRU.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 from matplotlib.backends.backend_pdf import PdfPages
 
-with open('../data/singleday.LRU.result') as f:
+with open('../data/200.singleday.LRU.result') as f:
 	lines = f.read().splitlines()
 
 dateList = []
