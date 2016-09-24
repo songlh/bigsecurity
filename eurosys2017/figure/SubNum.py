bff535af0cf520a6b929266d2ee608470b37df6d
Linhai <songlinhai0543@gmail.com>
2016-09-23 18:04:20 -0700

    check in updates in figure directory

--- eurosys2017/figure/SubNum.py
+++ eurosys2017/figure/SubNum.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 if __name__ == '__main__':
-	sDataFile = '../data/IDTotalFilterReputation.stat'
+	sDataFile = '../data/subTotalHisNum.stat'
 	fData = open(sDataFile, 'r')
 
 	XList = []
@@ -19,10 +19,7 @@ if __name__ == '__main__':
 
 		tmpList = line.split()
 
-		if float(tmpList[0]) == -1.0:
-			XList.append(-0.1)
-		else:
-			XList.append(float(tmpList[0]))
+		XList.append(int(tmpList[0]))
 
 		strTmp = ' '.join(tmpList[1:])
 		tupleStat = ast.literal_eval(strTmp)
@@ -38,13 +35,13 @@ if __name__ == '__main__':
 	fig, ax = plt.subplots()
 	ax.errorbar(XList, YList, yerr=errList, fmt='-o')
 
-	majorLocator = MultipleLocator(0.1)
+	majorLocator = MultipleLocator(10)
 	ax.xaxis.set_major_locator(majorLocator)
 
 	for tick in ax.xaxis.get_major_ticks():
 		tick.label.set_fontsize(18)
 
-	plt.xlabel('ID Reputation', fontsize=24)
+	plt.xlabel('Historical Submission Number', fontsize=24)
 
 	for tick in ax.yaxis.get_major_ticks():
 		tick.label.set_fontsize(18)  
@@ -53,8 +50,8 @@ if __name__ == '__main__':
 	plt.gcf().subplots_adjust(bottom=0.15)
 	plt.gcf().subplots_adjust(left=0.15)
 
-	xmin = XList[0]  - 0.05
-	xmax = XList[-1] + 0.05
+	xmin = XList[0]  - 5
+	xmax = XList[-1] + 5
 
 	ax.set_xlim(xmin, xmax)
 
@@ -62,14 +59,14 @@ if __name__ == '__main__':
 	ymax = 1.0
 	ax.set_ylim(ymin, ymax)
 
-	fig.canvas.draw()
+	#fig.canvas.draw()
 
-	labels = [item.get_text() for item in ax.get_xticklabels()]
-	labels[1] = '-1'
-	labels[2] = '0'
+	#labels = [item.get_text() for item in ax.get_xticklabels()]
+	#labels[1] = '-1'
+	#labels[2] = '0'
 
-	ax.set_xticklabels(labels)
+	#ax.set_xticklabels(labels)
 
-	#plt.show()
-	fig.savefig('IDReputation.pdf')
-	plt.close(fig)
+	plt.show()
+	#fig.savefig('IDReputation.pdf')
+	#plt.close(fig)
